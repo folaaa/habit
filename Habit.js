e1106231bcd43491c96846eda1c954d4e34877c1
folaaa <ofrankowska97@gmail.com>
2020-01-12 17:43:18 +0100

    add missing js files

---
+++ Habit.js
@@ -0,0 +1,33 @@
+class Habit {
+    constructor(name, goal, color, id, complete = 0) {
+      this.name = name;
+      this.goal = goal;
+      this.color = color;
+      this.id = id;
+      this.complete = complete;
+    }
+    addHabitToPage = () => {
+      habitContainer.innerHTML += `
+          <div id=${this.id} class="habit-show">
+              <h4 class="text-white">${this.name}</h4>
+              <div class="d-flex ">
+                  <div class="progress">
+                      <div class="progress-bar bg-${
+                        this.color
+                      }" role="progressbar" style="width: ${getProgress(
+        this.complete,
+        this.goal
+      )}" aria-valuenow="25"
+                          aria-valuemin="0" aria-valuemax="100"></div>
+                  </div>
+                  <button type="button" class="btn btn-dark btn-progress"><i class="fas fa-chevron-right"></i></button>
+                  <button type="button" class="btn btn-dark btn-delete"><i class="fas fa-times"></i></button>
+          
+              </div>
+              <p class="text-white"><span class ="complete">${
+                this.complete
+              }</span> / <span class="goal">${this.goal}</span></p>
+          </div>
+          `;
+    };
+  }
---
+++ Storage.js
@@ -0,0 +1,72 @@
+class Storage {
+    static getDate() {
+      let date = JSON.parse(localStorage.getItem("date"));
+      if (!date) {
+        date = new Date(
+          new Date().getFullYear(),
+          new Date().getMonth(),
+          new Date().getDate()
+        ).getTime();
+        this.updateDate(date);
+      }
+      return date;
+    }
+    static updateDate(newDate) {
+      localStorage.setItem("date", JSON.stringify(newDate));
+    }
+    static getHabitList() {
+      const habitList = JSON.parse(localStorage.getItem("habitList")) || [
+        {
+          name: "Learn something new",
+          goal: 2,
+          color: "success",
+          id: "8x7x0jmofan",
+          complete: 0
+        },
+        {
+          name: "Exercise",
+          goal: 1,
+          color: "warning",
+          id: "jfmggnomvlb",
+          complete: 0
+        }
+      ];
+      return habitList;
+    }
+    static updatHabitList(newHabitList) {
+      localStorage.setItem("habitList", JSON.stringify(newHabitList));
+    }
+  
+    static addHabit(habit) {
+      const habitList = this.getHabitList();
+      habitList.push(habit);
+      this.updatHabitList(habitList);
+    }
+    static removeHabit(id) {
+      const habitList = this.getHabitList();
+      habitList.forEach((habit, index) => {
+        if (habit.id == id) {
+          habitList.splice(index, 1);
+        }
+      });
+      this.updatHabitList(habitList);
+    }
+    static updateHabit(habit, complete) {
+      const habitList = this.getHabitList();
+      habitList.forEach((item, index) => {
+        if (item.name === habit.name) {
+          habit.complete = complete;
+          habitList.splice(index, 1, habit);
+        }
+      });
+      this.updatHabitList(habitList);
+    }
+    static resetComplete() {
+      const habitList = this.getHabitList();
+      habitList.forEach((habit, index) => {
+        habit.complete = 0;
+        habitList.splice(index, 1, habit);
+      });
+      this.updatHabitList(habitList);
+    }
+  }

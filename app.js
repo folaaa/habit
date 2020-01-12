1b50c1857ad0ffd1663d6d44a7bbea1bbdb44cea
folaaa <ofrankowska97@gmail.com>
2020-01-12 17:36:33 +0100

    add helpers.js

--- app.js
+++ app.js
@@ -7,26 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const repeatedNameAlert = form.querySelector("#repeated-name-alert");
     const wrongDataTypeAlert = form.querySelector("#wrong-data-type-alert");
 
-const toggleClasses = (element, ...classNames) => {
-    // If the class exists, remove it, if not, then add it
-    classNames.forEach(className => element.classList.toggle(className));
-  };
-  const closeAlerts = () => {
-    document.querySelectorAll(".alert").forEach(alert => {
-      if (alert.classList.contains("alert-show")) {
-        toggleClasses(alert, "alert-hide", "alert-show");
-      }
-    });
-  };
-  const getProgress = (complete, goal) => {
-    return `${(complete / goal) * 100}%`;
-  };
-  
-  const generateId = () => {
-    return Math.random()
-      .toString(36)
-      .substr(2, 16);
-  };
 
 class Habit {
     constructor(name, goal, color, id, complete = 0) {
---
+++ helpers.js
@@ -0,0 +1,20 @@
+const toggleClasses = (element, ...classNames) => {
+    // If the class exists, remove it, if not, then add it
+    classNames.forEach(className => element.classList.toggle(className));
+  };
+  const closeAlerts = () => {
+    document.querySelectorAll(".alert").forEach(alert => {
+      if (alert.classList.contains("alert-show")) {
+        toggleClasses(alert, "alert-hide", "alert-show");
+      }
+    });
+  };
+  const getProgress = (complete, goal) => {
+    return `${(complete / goal) * 100}%`;
+  };
+  
+  const generateId = () => {
+    return Math.random()
+      .toString(36)
+      .substr(2, 16);
+  };
